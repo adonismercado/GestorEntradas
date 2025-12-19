16395ed03bf0131ecb618b028d1e8dd6aa6a22e0
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-19 13:54:31 -0400

    feat ✨: Agrega modelo Producto

---
+++ GestorEntradas/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorEntradas.Models;
+
+public class Producto
+{
+    [Key]
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage = "La descripcion del producto es obligatoria.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor que 0.")]
+    [Required(ErrorMessage = "El costo es obligatorio.")]
+    public double Costo { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0.")]
+    [Required(ErrorMessage = "El precio es obligatorio.")]
+    public double Precio { get; set; }
+
+    public int Existencia { get; set; }
+}
